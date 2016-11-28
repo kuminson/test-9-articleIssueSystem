95b7dda8dc8c04ea0fa8ec57aaf6ad50f3f5f0f2
kumin <273423480@qq.com>
2016-11-28 08:46:55 +0800

    finished the issue article page

---
+++ php/config/config.php
@@ -0,0 +1,8 @@
+<?php
+	// 服务器IP地址
+	define('HOST','127.0.0.1');
+	// 服务器用户名
+	define('USERNAME','root');
+	// 服务器密码
+	define('PASSWORD','');
+?>
---
+++ php/connect/connect.php
@@ -0,0 +1,21 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'].'/php/config/config.php');
+	// 连库
+	if(!($con = mysqli_connect(HOST,USERNAME,PASSWORD))){
+		echo '<br/>';
+		echo '连接失败';
+		echo mysqli_error();
+	}
+	// 选库
+	if(!(mysqli_select_db($con,'article'))){
+		echo '<br/>';
+		echo '选择数据库失败';
+		echo mysqli_error();
+	}
+	// 字符集
+	if(!(mysqli_query($con,'SET NAMES gbk'))){
+		echo '<br/>';
+		echo '修改字符集失败';
+		echo mysqli_error();
+	}
+?>
